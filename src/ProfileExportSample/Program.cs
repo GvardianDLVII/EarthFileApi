bbc9ea4929c43973e4227a12fa3f15eceadad4bc
GvardianDLVII <przemeksz557@gmail.com>
2023-05-04 00:39:15 +0200

    sample and simple profile conversion tool

---
+++ src/ProfileExportSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ieo.EarthFileApi.Files;
+using System.Text.Json;
+
+namespace LevelSaveSample
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         if(args.Length != 1)
+         {
+            Console.WriteLine("Please provide a single argument with path to UserInfo.dat");
+         }
+         var profileFile = EarthFileReader.ReadProfileFile(args[0]);
+
+         File.WriteAllText("UserInfo.json", JsonSerializer.Serialize(profileFile));
+      }
+   }
+}
